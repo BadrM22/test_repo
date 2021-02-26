a4a916c4f1295b00b223b5c3d2c5c4c971bb7403
Mohammed Badr <badrm0279@gmail.com>
2021-02-26 12:50:17 +0200

    script to print test
    python script to check printing

---
+++ first_test.py
@@ -0,0 +1,13 @@
+Python 3.9.1 (v3.9.1:1e5d33e9b9, Dec  7 2020, 12:10:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> print("Hello")
+Hello
+>>> x=5
+>>> print(x)
+5
+>>> print("Hello")
+Hello
+>>> 
